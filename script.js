5dcc88da2693787d80c07bf35ad03d24769e2ec0
Utsav Shahi <92775677+shahiutsav@users.noreply.github.com>
2024-08-02 09:43:41 +0545

    Implement responsiveness and interactions

--- script.js
+++ script.js
@@ -212,4 +212,29 @@ copyButton.addEventListener("click", () => {
   )}, ${discordFormatR} which is ${discordFormatF} your local time. \nSend a deposit of 2 Edvd's to secure your jump. \n\n0/7 Slots Filled\n0/1 Priority Slots Filled`;
 
   navigator.clipboard.writeText(textToCopy);
+
+  copyButton.innerHTML = "Copied!";
+  copyButton.classList.add("copied");
+  copyButton.disabled = true;
+
+  setTimeout(() => {
+    copyButton.classList.remove("copied");
+    copyButton.disabled = false;
+    copyButton.innerHTML = `Copy Text <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#1d2021"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+          <path
+            d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"
+          ></path>
+        </svg>`;
+  }, 1000);
 });
